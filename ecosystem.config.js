bfe67c33a62d95031af0e6e6a01795520f913ec6
xkux8023 <xkux8023@github.com>
2018-07-12 00:08:39 +0800

    update ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  apps : [{
+    name      : 'todo-demo',
+    script    : 'server.js',
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production : {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : 'xkux8023.cn',
+      ref  : 'origin/master',
+      repo : 'git@github.com:xkux8023/todo-demo.git',
+      path : '/var/www/production',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
